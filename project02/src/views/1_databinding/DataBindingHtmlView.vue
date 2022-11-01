badca3f2f17f3a3792457303f8cc0a0c6e95b839
장두혁 <jjd0324@gmail.com>
2022-11-01 17:58:11 +0900

    init today study: vue.js dataBinding

---
+++ project02/src/views/1_databinding/DataBindingHtmlView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>{{ htmlString }}</div>
+    <h1 v-html="htmlString"></h1>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      htmlString: '<p style="color:red;">Red</p>'
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
--- project02/src/views/1_databinding/DataBindingStringView.vue
+++ project02/src/views/1_databinding/DataBindingStringView.vue
@@ -1,21 +1,25 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <h1>hello {{ userName }}</h1>
+    <p>{{ message }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            userName: 'duho'
-        }
-    },
+  data() {
+    return {
+      userName: 'duho',
+      message: 'welcome!',
+      arr: [],
+      obj: {}
+    }
+  }
 }
 </script>
 
 <style scoped>
 .text-red {
-    color: red;
+  color: red;
 }
 </style>
